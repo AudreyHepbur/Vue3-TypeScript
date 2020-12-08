2baa103de008909a8829e53bb53791fc6f65956b
wangning <973575123@qq.com>
2020-12-08 17:51:24 +0800

    🎉 vue ui、jest、ts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <img alt="Vue logo"
+         src="./assets/logo.png" />
+    <div>
+      <h2>欢迎光临红浪漫洗浴中心</h2>
+      <div>请选择一位美女为你服务</div>
+    </div>
+    <div>
+      <button v-for="(item, index) in girls"
+              :key="index"
+              @click="selectGirlFun(index)">
+        {{item}}
+      </button>
+      <div>你选择了【{{ selectGirl }}】为你服务</div>
+    </div>
   </div>
-  <router-view/>
 </template>
-
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const girls = ref(["莫娜", "琴", "凝光"]);
+    const selectGirl = ref("");
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index];
+    };
+    return {
+      girls,
+      selectGirl,
+      selectGirlFun,
+    };
+  },
+});
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
